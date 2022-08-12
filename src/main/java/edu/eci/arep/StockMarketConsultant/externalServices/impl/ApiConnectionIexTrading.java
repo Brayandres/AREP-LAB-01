cad96254a7dde4ff031b23f1f43fa8c65c1ed7fc
Brayan Macías <brayandresmt@outlook.com>
2022-08-12 14:42:43 -0500

    Implementing Service exchange - Initial

---
+++ src/main/java/edu/eci/arep/StockMarketConsultant/externalServices/impl/ApiConnectionIexTrading.java
@@ -0,0 +1,12 @@
+package edu.eci.arep.StockMarketConsultant.externalServices.impl;
+
+import edu.eci.arep.StockMarketConsultant.externalServices.ApiConnection;
+import edu.eci.arep.StockMarketConsultant.externalServices.TimeFrame;
+
+public class ApiConnectionIexTrading implements ApiConnection {
+    
+    @Override
+    public String getStockValuationHistory(String stockName, TimeFrame timeFrame) {
+        return null;
+    }
+}
