0e39bad1e2525c83c83ffd21d1c378f9ac0b2583
Brayan Macías <brayandresmt@outlook.com>
2022-08-14 17:08:52 -0500

    Setting 'static' folder feature for HTTP web pages

--- src/main/java/edu/eci/arep/StockMarketConsultant/SparkWebServer.java
+++ src/main/java/edu/eci/arep/StockMarketConsultant/SparkWebServer.java
@@ -5,6 +5,7 @@ import static spark.Spark.*;
 public class SparkWebServer {
 
     public static void main(String[] args) {
+        staticFileLocation("/static");
         port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
     }
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,3 @@
+var ApiClient = (function () {
+	// body...
+})();
